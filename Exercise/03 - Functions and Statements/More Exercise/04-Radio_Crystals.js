85cc5180fb0abd7d769212b175cc4edfc864f037
iliyan <iliyan.hr.iliev@gmail.com>
2024-03-11 23:28:58 +0000

    Update some files

--- Exercise/03 - Functions and Statements/More Exercise/04-Radio_Crystals.js
+++ Exercise/03 - Functions and Statements/More Exercise/04-Radio_Crystals.js
@@ -1,49 +1,57 @@
 function solve(arrayOfNumbers) {
-    let result = []
-    let finalThickness = arrayOfNumbers.shift()
+    let result = [];
+    let finalThickness = arrayOfNumbers.shift();
+    let operationsArray = ['Cut', 'Lap', 'Grind', 'Etch'];
     let operations = {
         'Cut': x => x / 4,
         'Lap': x => x * 0.8,
         'Grind': x=> x - 20,
         'Etch': x => x - 2,
-        'X-ray': x => x + 1,
     }
-
-    let operationsArray = ['Cut', 'Lap', 'Grind', 'Etch', 'X-ray']
+    let xRay =  x => x + 1;
+    let wash = x => Math.floor(x);
 
     for (let element of arrayOfNumbers) {
-        result.push(`Processing chunk ${element} microns`)
-        if (Math.floor(element) === finalThickness) {
-            result.push('Transporting and washing')
-            element = Math.floor(element)
-        }
-        for (const operation of operationsArray) {
-            let operationCount = 0
-            while (element > finalThickness ) {
+    result.push(`Processing chunk ${element} microns`);
 
-                if (operations[operation](element) < finalThickness - 1) {
-                    break
-                }
-                operationCount += 1
-                element = operations[operation](element)
-            }
-            if (operationCount === 0) {
-                break
-            }
-
-            result.push(`${operation} x${operationCount}`)
-
-            result.push('Transporting and washing')
-            element = Math.floor(element)
-        }
-        if (element < finalThickness) {
-            element = operations['X-ray'](element)
-            result.push('X-ray x1')
-        }
-        result.push(`Finished crystal ${element} microns`)
+    if (finalThickness === wash(element)) {
+        result.push('Transporting and washing');
+        element = wash(element);
     }
 
-    console.log(result.join('\n'))
+        for (const operation of operationsArray) {
+            let operationCount = 0;
+
+            while (element > finalThickness) {
+
+                if (operations[operation](element) < finalThickness ) {
+                    break;
+                }
+
+                operationCount += 1;
+                element = operations[operation](element);
+            }
+
+            if (operationCount === 0) {
+                continue;
+            }
+
+            result.push(`${operation} x${operationCount}`);
+            element = wash(element);
+            result.push('Transporting and washing');
+        }
+
+        if (element < finalThickness) {
+            element = xRay(element);
+            result.push('X-ray x1');
+        }
+
+        result.push(`Finished crystal ${element} microns`);
+    }
+
+    console.log(result.join('\n'));
 }
 
-solve([1375, 50000, 1375.5, 10])
+// solve([100, 101.9])
+
+solve([100, 99])
