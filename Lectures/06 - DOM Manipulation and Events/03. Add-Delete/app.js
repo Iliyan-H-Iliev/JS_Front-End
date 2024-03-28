3a9459f417c2a743fdcefe3f372625678aad24d8
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-03-28 23:32:05 +0000

    Update JS files

--- Lectures/06 - DOM Manipulation and Events/03. Add-Delete/app.js
+++ Lectures/06 - DOM Manipulation and Events/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
+    const ulElement = document.getElementById('items')
+    const newItemTextElement = document.getElementById('newItemText')
+    const liElement = document.createElement('li')
 
+    liElement.textContent = newItemTextElement.value
+    const aElement = document.createElement('a')
+    aElement.href = '#'
+    aElement.textContent = '[Delete]'
+
+    aElement.addEventListener('click', () => {
+        liElement.remove()
+    })
+
+    liElement.appendChild(aElement)
+    ulElement.appendChild(liElement)
+
+    newItemTextElement.value = ' '
 }
--- Lectures/06 - DOM Manipulation and Events/04. Mouse-Gradient/app.js
+++ Lectures/06 - DOM Manipulation and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
+    const gradientElement = document.getElementById('gradient')
+    const resultElement = document.getElementById('result')
 
+    gradientElement.addEventListener('mousemove', (ev) => {
+        const currentWidth = ev.offsetX
+        const elementWidth = ev.target.clientWidth
+        const percentage = Math.floor((currentWidth / elementWidth) * 100)
+        resultElement.textContent = `${percentage}%`
+    })
 }
