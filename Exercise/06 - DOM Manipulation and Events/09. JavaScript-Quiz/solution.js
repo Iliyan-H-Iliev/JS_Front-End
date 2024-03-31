764620db6bea9c43d96772aec6fe635fcd7b26d9
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-03-31 16:52:27 +0100

    Update JS files

--- Exercise/06 - DOM Manipulation and Events/09. JavaScript-Quiz/solution.js
+++ Exercise/06 - DOM Manipulation and Events/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-  //TODO...
+    const sectionsUlElements = document.querySelectorAll('section ul')
+    const resultElement = document.querySelector('.results-inner')
+    const givenAnswers = []
+
+    const answers = {
+        'onclick': true,
+        'JSON.stringify()': true,
+        'A programming API for HTML and XML documents': true,
+        'onmouseclick': false,
+        'JSON.toString()': false,
+        'The DOM is your source HTML': false,
+    }
+
+    Array.from(sectionsUlElements).forEach((ulElement, i) => {
+        ulElement.addEventListener('click', (ev) => {
+            if (!ev.target.className.includes('answer-')) {
+                return;
+            }
+            const parentElement = ulElement.parentElement
+            givenAnswers.push(answers[ev.target.textContent])
+            parentElement.classList.add('hidden')
+            parentElement.style.display = 'none'
+
+            if (i === sectionsUlElements.length - 1) {
+                const allCorrectAnswers = givenAnswers.filter(bool => bool === true)
+                resultElement.parentElement.style.display = 'block'
+
+                resultElement.querySelector('h1').textContent = allCorrectAnswers.length === sectionsUlElements.length
+                    ? 'You are recognized as top JavaScript fan!'
+                    : `You have ${allCorrectAnswers.length} right answers`
+
+                return;
+            }
+            parentElement.nextElementSibling.classList.remove('hidden')
+            parentElement.nextElementSibling.style.display = 'block'
+        })
+    })
 }
