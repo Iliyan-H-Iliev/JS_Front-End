e5e545d5d159f32964246b025d69c18acf707ca1
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2024-03-31 02:41:54 +0100

    Update JS files

--- Exercise/06 - DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
+++ Exercise/06 - DOM Manipulation and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const convertButtonElement = document.getElementById('convert')
+    const inputDistanceElement = document.getElementById('inputDistance')
+    const inputUnitsElement = document.getElementById('inputUnits')
+    const outputDistanceElement = document.getElementById('outputDistance')
+    const outputUnitsElement = document.getElementById('outputUnits')
+
+    const toMeters = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+    }
+
+    convertButtonElement.addEventListener('click', () => {
+        const inputDistance = inputDistanceElement.value
+        const inputUnit = inputUnitsElement.value
+        const outputUnit = outputUnitsElement.value
+
+        outputDistanceElement.value = (Number(inputDistance) * toMeters[inputUnit]) / toMeters[outputUnit]
+    })
 }
--- Exercise/06 - DOM Manipulation and Events/08. Sudomu/solution.js
+++ Exercise/06 - DOM Manipulation and Events/08. Sudomu/solution.js
@@ -1,4 +1,82 @@
 function solve() {
+    const tableElement = document.querySelector('table')
+    const quickCheckButtonElement = document.querySelector('tfoot button:first-of-type')
+    const clearButtonElement = document.querySelector('tfoot button:last-of-type')
+    const resultMessageElement = document.querySelector('#check p')
+    const trElements = document.querySelectorAll('tbody tr')
 
-    //TODO...
+
+    const result = {
+        true: () => {
+            resultMessageElement.textContent = "You solve it! Congratulations!"
+            resultMessageElement.style.color = 'green'
+            tableElement.style.border = "2px solid green"
+
+        },
+        false: () => {
+            resultMessageElement.textContent = "NOP! You are not done yet..."
+            resultMessageElement.style.color = 'red'
+            tableElement.style.border = "2px solid red"
+        },
+        clear: () => {
+            resultMessageElement.textContent = ""
+            resultMessageElement.style.color = 'none'
+            tableElement.style.border = ""
+        },
+    }
+
+    quickCheckButtonElement.addEventListener('click', () => {
+        result.clear()
+        const sudokoMatrix = []
+        let isCorrect = true
+
+
+        for (const trElement of trElements) {
+            const row = []
+            const numbersAsString = Array.from(trElement.querySelectorAll('input[type=number]'))
+
+            for (const number of numbersAsString) {
+
+                if (number.value === '' || Number(number.value) < 1 || Number(number.value) > numbersAsString.length) {
+                    isCorrect = false
+                    break
+                }
+
+                row.push(Number(number.value))
+            }
+
+            if (!isCorrect || row.length !== new Set(row).size) {
+                isCorrect = false
+                break
+            }
+            sudokoMatrix.push(row)
+        }
+
+        if (!isCorrect) {
+            result[isCorrect]()
+            return;
+        }
+
+        for (let i = 0; i < sudokoMatrix.length - 1; i++) {
+            for (let j = 0; j < sudokoMatrix[i].length; j++) {
+                if (sudokoMatrix[i][j] === sudokoMatrix[i + 1][j]) {
+                    isCorrect = false
+                    break
+                }
+                if (!isCorrect) {
+                    result[isCorrect]()
+                    return;
+                }
+            }
+
+        }
+
+        result[isCorrect]()
+    })
+
+    clearButtonElement.addEventListener('click', () => {
+        Array.from(tableElement.querySelectorAll('input[type=number]')).forEach(cell => cell.value = '')
+        result.clear()
+    })
+
 }
